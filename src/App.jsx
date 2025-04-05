733df100f0b53762d69e38ed35ed9af7bcfe7fb0
ValeriyVinn <valeriy.all@gmail.com>
2025-04-05 13:40:34 +0300

    Test menu 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import SharedLayout from "./components/SharedLayout/SharedLayout";
-// import VanillaMenu from "./components/VanillaMenu/VanillaMenu";
+import VanillaMenu from "./components/VanillaMenu/VanillaMenu";
 
 function App() {
   return (
@@ -9,8 +9,8 @@ function App() {
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<h1>Home</h1>} />
        <Route path="/exercises" element={<h1>Exercises</h1>}></Route>
-      <Route path="/vanilla-js" element={<h1>Vanilla</h1>}></Route>
-      {/* <Route path="/vanilla-js" element={<VanillaMenu/>}></Route> */}
+      {/* <Route path="/vanilla-js" element={<h1>Vanilla</h1>}></Route> */}
+      <Route path="/vanilla-js" element={<VanillaMenu/>}></Route>
       <Route path="/react" element={<h1>React</h1>}></Route>
       <Route path="/redux" element={<h1>Redux</h1>}></Route>
       <Route path="/node-js" element={<h1>Node JS</h1>}></Route>
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/exercises/",
-  // server: {
-  //   historyApiFallback: true, // щоб Vite не давав 404 при перезавантаженні сторінки
-  // },
-});
-// export default defineConfig(({ mode }) => ({
+// export default defineConfig({
 //   plugins: [react()],
-//   base: mode === "production" ? "/exercises/" : "/",
-// }));
+//   base: "/exercises/",
+//   // server: {
+//   //   historyApiFallback: true, // щоб Vite не давав 404 при перезавантаженні сторінки
+//   // },
+// });
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === "production" ? "/exercises/" : "/",
+}));
