ffd2dc4bc344dbb71b0af2cff7067e5bb02f8694
ValeriyVinn <valeriy.all@gmail.com>
2025-04-12 19:28:50 +0300

    Test3

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/exercises/",
-//   // server: {
-//   //   historyApiFallback: true, // щоб Vite не давав 404 при перезавантаженні сторінки
-//   // },
-// });
-
-
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === "production" ? "/exercises/" : "/",
-
+  // Додаємо обробку змінних середовища
   define: {
     "process.env": process.env,
   },
