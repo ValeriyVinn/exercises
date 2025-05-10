76bc0b54de8eb3e23ee2b1567c4bbea0ee90e233
ValeriyVinn <valeriy.all@gmail.com>
2025-05-11 00:04:59 +0300

    Updates

--- vanilla/05-working-with-the-dom/01-element-selection/toggle-background-color/toggle-background-color.js
+++ vanilla/05-working-with-the-dom/01-element-selection/toggle-background-color/toggle-background-color.js
@@ -1,7 +1,7 @@
 import { setupExercise } from "../../../scripts/vanilla-exercise-handler.js";
 
 setupExercise({
-  fileKey: "05-working-with-the-DOM",
+  fileKey: "05-working-with-the-dom",
   exerciseName: "Toggle background color",
   statement:
     "There are several buttons on a page. Write a script that toggled the color of the button to yellow or antiquewhite when clicked.",
