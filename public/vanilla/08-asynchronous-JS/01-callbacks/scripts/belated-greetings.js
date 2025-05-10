83ec2b25b5f0601d075ecac551f98818466fb5fa
ValeriyVinn <valeriy.all@gmail.com>
2025-05-11 00:03:52 +0300

    Fixed names of files

--- public/vanilla/08-asynchronous-JS/01-callbacks/scripts/belated-greetings.js
+++ public/vanilla/08-asynchronous-JS/01-callbacks/scripts/belated-greetings.js
@@ -1,12 +1,14 @@
 import { setupExercise } from "../../../scripts/vanilla-exercise-handler.js";
 
 setupExercise({
-  fileKey: "08-asynchronous-JS",
+  fileKey: "08-asynchronous-js",
   exerciseName: "Belated greetings",
   statement:
     "Write a function greet(name, callback) that takes a name and a callback function. Print a 'Greeting complete!' after 3 seconds.",
 });
-
+// 09.05.2025 - 20:25 --score is: 6:39
+// 09.05.2025 - 20:40 --score is: 4:30
+// 10.05.2025 - 21:10 --score is: 6:37
 // ! Рішення ----------
 function greet(name, callback) {
   console.log(`Hello, ${name}!`);
@@ -16,5 +18,5 @@ function greet(name, callback) {
 }
 
 greet("Alex", () => {
-  console.log("Greeting complete!");
+  console.log("Sorry, time is up, you have to go.");
 });
--- public/vanilla/08-asynchronous-JS/01-callbacks/scripts/processing-an-array-with-callback.js
+++ public/vanilla/08-asynchronous-JS/01-callbacks/scripts/processing-an-array-with-callback.js
@@ -1,12 +1,13 @@
 import { setupExercise } from "../../../scripts/vanilla-exercise-handler.js";
 
 setupExercise({
-  fileKey: "08-asynchronous-JS",
+  fileKey: "08-asynchronous-js",
   exerciseName: "Processing an array with callback",
   statement:
     "Write a function processArray(arr, callback) that processes each element of the array through a callback function.",
 });
-
+// 09.05.2025 - 20:40 --score is: 20:13
+// 10.05.2025 - 21:30 --score is: 06:57
 // ! Рішення ----------
 function processArray(arr, callback) {
   arr.forEach((item, index) => {
