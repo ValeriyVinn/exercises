cf404a39fab88cac73cff99cc7a74f2e2ea4bf7d
ValeriyVinn <valeriy.all@gmail.com>
2025-05-26 01:28:11 +0300

    Updates

--- vanilla/10-date-and-time/01-date-object/countdown-timer-to-event/countdown-timer-to-event.js
+++ vanilla/10-date-and-time/01-date-object/countdown-timer-to-event/countdown-timer-to-event.js
@@ -7,7 +7,7 @@ setupExercise({
 });
 
 // ! Рішення ----------
-// const eventDate = new Date("2025-06-01T00:00:00");
+// const eventDate = new Date("2025-05-24 21:40:00");
 
 // function countdown() {
 //   const now = new Date();
@@ -28,3 +28,28 @@ setupExercise({
 // }
 
 // const timer = setInterval(countdown, 1000);
+
+// const eventDate = new Date("2025-05-24 22:58:00");
+
+// const el = document.getElementById("countdown-timer");
+// console.log(el.textContent);
+
+// function countdown() {
+//   const now = new Date();
+//   const diff = eventDate - now;
+
+//   if (diff <= 0) {
+//     clearInterval(timer);
+//     return "Торба";
+//   }
+
+//   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+//   const minutes = Math.floor((diff / (1000 * 60)) % 60);
+//   const seconds = Math.floor((diff / 1000) % 60);
+
+//   return `${String(days).padStart(2, "0")}:${String(hours).padStart(2,"0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+// }
+// const timer = setInterval(() => {
+//   el.textContent = countdown();
+// }, 1000);
