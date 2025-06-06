89824d10992a71e85e880c343dc0066671fe93df
ValeriyVinn <valeriy.all@gmail.com>
2025-06-06 20:03:12 +0300

    Added path selection function in SharedLayout

--- src/layouts/SharedLayout.jsx
+++ src/layouts/SharedLayout.jsx
@@ -1,6 +1,66 @@
+// import { NavLink, Outlet } from "react-router-dom";
+// import css from "./SharedLayout.module.css";
+// import ReactMenu from "./ReactMenu"; // Імпортуємо меню React
+
+// const SharedLayout = () => {
+//   return (
+//     <div>
+//       <ul className={css.navList}>
+//         <li className={css.navItem}>
+//           <NavLink to="/" className={css.navLink}>
+//             ToDoList
+//           </NavLink>
+//         </li>
+//         <li className={css.navItem}>
+//           <a href="/vanilla/html-css/html-css.html" className={css.navLink}>
+//             HtmlCss
+//           </a>
+//         </li>
+//         <li className={css.navItem}>
+//           <a href="/vanilla/java-script/java-script.html" className={css.navLink}>
+//             JavaScript
+//           </a>
+//         </li>
+//         <li className={css.navItem}>
+//           <NavLink to="react" className={css.navLink}>
+//             React
+//           </NavLink>
+//           {/* Додаємо меню прямо тут */}
+//           <div className={css.reactMenu}>
+//             <ReactMenu />
+//           </div>
+//         </li>
+//         <li className={css.navItem}>
+//           <NavLink to="redux" className={css.navLink}>
+//             Redux
+//           </NavLink>
+//         </li>
+//         <li className={css.navItem}>
+//           <NavLink to="node-js" className={css.navLink}>
+//             NodeJS
+//           </NavLink>
+//         </li>
+//       </ul>
+//       <main>
+//         <Outlet />
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default SharedLayout;
+
+
 import { NavLink, Outlet } from "react-router-dom";
 import css from "./SharedLayout.module.css";
-import ReactMenu from "./ReactMenu"; // Імпортуємо меню React
+import ReactMenu from "./ReactMenu";
+
+const isLocalhost =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1";
+
+const getVanillaHref = (path) => {
+  return isLocalhost ? `/vanilla/${path}` : `vanilla/${path}`;
+};
 
 const SharedLayout = () => {
   return (
@@ -12,20 +72,25 @@ const SharedLayout = () => {
           </NavLink>
         </li>
         <li className={css.navItem}>
-          <NavLink to="html-css" className={css.navLink}>
+          <a
+            href={getVanillaHref("html-css/html-css.html")}
+            className={css.navLink}
+          >
             HtmlCss
-          </NavLink>
+          </a>
         </li>
         <li className={css.navItem}>
-          <NavLink to="vanilla-js" className={css.navLink}>
+          <a
+            href={getVanillaHref("java-script/java-script.html")}
+            className={css.navLink}
+          >
             JavaScript
-          </NavLink>
+          </a>
         </li>
         <li className={css.navItem}>
           <NavLink to="react" className={css.navLink}>
             React
           </NavLink>
-          {/* Додаємо меню прямо тут */}
           <div className={css.reactMenu}>
             <ReactMenu />
           </div>
