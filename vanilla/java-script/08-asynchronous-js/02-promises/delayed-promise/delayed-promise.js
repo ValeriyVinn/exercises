4a5fd468c31dcbb47c4d11ed982d2e8dffe4d4b8
ValeriyVinn <valeriy.all@gmail.com>
2025-06-11 22:27:15 +0300

    Updates

--- vanilla/java-script/08-asynchronous-js/02-promises/delayed-promise/delayed-promise.js
+++ vanilla/java-script/08-asynchronous-js/02-promises/delayed-promise/delayed-promise.js
@@ -8,87 +8,21 @@ setupExercise({
 });
 
 // ! Рішення ----------
-// const isSuccess = false;
-// function delay(ms) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (isSuccess) {
-//         resolve("1 second passed");
-//       } else {
-//         reject("Sorry, it's Error");
-//       }
-//     }, ms)
-//   });
-// }
-
-// delay(3000)
-//   .then(value => console.log(value))
-//   .catch(error => console.error(error));
-
-
-// const isSuccess = false;
-
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     if (isSuccess) {
-//       resolve("Success! Value passed to resolve function");
-//     } else {
-//       reject("Error! Error passed to reject function");
-//     }
-//   }, 2000);
-// });
-
-// promise
-//   .then(value => {
-//     console.log(value);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
-
-// const fetchUserFromServer = (username, onSuccess, onError) => {
-//   console.log(`Fetching data for ${username}`);
-
-//   setTimeout(() => {
-//     // Change value of isSuccess variable to simulate request status
-//     const isSuccess = false;
-
-//     if (isSuccess) {
-//       onSuccess("Everything went well.");
-//     } else {
-//       onError("Nothing to say.");
-//     }
-//   }, 2000);
-// };
-
-// const onFetchSuccess = (user) => {
-//   console.log(user);
-// };
-
-// const onFetchError = (error) => {
-//   console.error(error);
-// };
-
-// fetchUserFromServer("Mango", onFetchSuccess, onFetchError);
-
 
 // const fetchUserFromServer = (username) => {
 //   return new Promise((resolve, reject) => {
-//     console.log(`Fetching data for ${username}`);
-
+//     console.log(`I promise you, ${username}`);
+    
 //     setTimeout(() => {
-//       // Change value of isSuccess variable to simulate request status
-//       const isSuccess = true;
-
+//      const isSuccess = Math.random() > 0.5; 
 //       if (isSuccess) {
-//         resolve("Everything went well, Chunga.");
+//         resolve("Here's a star for you, Chunga!");
 //       } else {
-//         reject("Nothing to say about Chunga.");
+//         reject("Sorry, all the stars have gone out, Chunga!");
 //       }
-//     }, 2000);
+//     }, 3000);
 //   });
 // };
-
-// fetchUserFromServer("Chunga-changa")
+// fetchUserFromServer("Chunga")
 //   .then((user) => console.log(user))
 //   .catch((error) => console.error(error));
